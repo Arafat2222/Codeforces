c9ca7e684694d2ea2e75ebc5678590623a4d69f1
Md. Arafat Kbir <99961325+Arafat2222@users.noreply.github.com>
2023-06-19 14:28:28 +0600

    Create 1836A.cpp

---
+++ 1836A.cpp
@@ -0,0 +1,79 @@
+///\/\/\/\/\/***************الله أكبر***************/\/\/\/\/\/\/
+//*********** حَسبنا الله و نعم الوکیل نعم المولی و نعم النصیر *************
+
+//1. بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
+//2. الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ
+//3. الرَّحْمَٰنِ الرَّحِيمِ
+//4. مَالِكِ يَوْمِ الدِّينِ
+//5. إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ
+//6. اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ
+//7. صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ
+
+								
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define isVolwel(ch ) (0x208222 >> (ch & 0x1f)) & 1
+#define full(s) s.begin(), s.end()
+#define Size int(1e7)
+
+
+vector<ll> Prime;
+int isprime(ll N){
+    if(N<2 || (!(N&1) && N!=2))
+        return -1;
+    for(int i=3; i*i<= sqrt(N); i+=2){
+        if(!(N%i))
+            return -1;
+    }
+    return 1;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0),cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        map<int, int> mp;
+        loop(i, n){
+            cin >> v[i];
+            mp[v[i]]++;
+        }
+        int f = 1;
+        int sz = mp.size();
+        for (int i = 0; i < mp.size(); i++){
+            if(mp[i]== 0){
+                cout << "NO\n";
+                f = 0;
+                break;
+            }
+        }
+        if(f){
+            int c = 1;
+            for (int i = 0; i < sz - 1; i++){
+                if((mp[i] >= mp[i+1])){   
+                    c = 1;
+                }
+                else{
+                    c = 0;
+                    cout << "NO\n";
+                    break;
+                }
+            }
+            if(c){
+                cout << "YES\n";
+            }
+
+        }
+    }
+}
+//			 @@MD.Arafat Kabir@@	
+///\/\/\/\/\**********@University of Asia Pacific@**********/\/\/\/\/\/
