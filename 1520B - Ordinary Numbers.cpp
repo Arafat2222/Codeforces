a3ea0917f2f13f27c93548ca9766047f25f0e4d3
Md. Arafat Kbir <99961325+Arafat2222@users.noreply.github.com>
2023-07-10 16:34:42 +0600

    Accept

---
+++ 1520B - Ordinary Numbers.cpp
@@ -0,0 +1,70 @@
+///\/\/\/\/\/***************الله أكبر***************/\/\/\/\/\/\/
+//*********** حَسبنا الله و نعم الوکیل نعم المولی و نعم النصیر *************
+ 
+//1. بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
+//2. الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ
+//3. الرَّحْمَٰنِ الرَّحِيمِ
+//4. مَالِكِ يَوْمِ الدِّينِ
+//5. إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ
+//6. اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ
+//7. صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ
+
+#include <bits/stdc++.h>
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define FOR(i, a, b) for (int i = a; i < b;i++)
+#define cin_v(v,n) loop(i,n) cin >> v[i]
+#define cout_v(v,n) loop(i,n) cout << v[i] << " "
+#define full(v) v.begin(),v.end()
+#define ll long long
+using namespace std;
+int main()
+{
+		ios_base::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+		vector<ll> v;
+		for (ll i = 1; i <= 9; i++){
+			v.push_back(i);
+		}
+		for (ll i = 11; i <= 99; i+=11)
+		{
+			v.push_back(i);
+		}
+		for (ll i = 111; i <= 999;i+=111){
+			v.push_back(i);
+		}
+		for (ll i = 1111; i <= 9999;i+=1111){
+			v.push_back(i);
+		}
+		for (ll i = 11111; i <= 99999; i+=11111){
+			v.push_back(i);
+		}
+		for (ll i = 111111; i <= 999999; i+=111111){
+			v.push_back(i);
+		}
+		for (ll i = 1111111; i <= 9999999; i+=1111111){
+			v.push_back(i);
+		}
+		for (ll i = 11111111; i <= 99999999; i+=11111111){
+			v.push_back(i);
+		}
+		for (ll i = 111111111; i <= 999999999;i+=111111111){
+			v.push_back(i);
+		}
+
+		// for (ll i = 0; i < v.size();i++){
+		// 	cout << v[i] << "\n";
+		// }
+		int t;
+		cin >> t;
+		while(t--){
+			ll n;
+			cin >> n;
+			int c = 0;
+			for (int i = 0; v[i] <= n;i++){
+				c++;
+			}
+			cout << c << "\n";
+		}
+}
+//			 @@MD.Arafat Kabir@@	
+///\/\/\/\/\**********@University of Asia Pacific@**********/\/\/\/\/\/
